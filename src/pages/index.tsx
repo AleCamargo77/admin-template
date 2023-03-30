4da67836464e4e87c71436f22f911e1cc6fd895a
Alessandro Camargo <acamargodias@terra.com.br>
2023-03-30 08:25:33 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className={`bg-gradient-to-r from-green-500 to-blue-500`}>
+      <h1>Teste</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [
+    "./src/components/**.*{js,jsx,ts,tsx,vue}",
+    "./src/pages/**.*.{js,jsx,ts,tsx,vue}",
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
