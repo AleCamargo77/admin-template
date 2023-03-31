0c6cdc734afc6d3621e938b83c452b8b93713a6b
Alessandro Camargo <acamargodias@terra.com.br>
2023-03-31 09:35:25 -0300

    tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className={`bg-gradient-to-r from-green-500 to-blue-500`}>
-      <h1>Teste</h1>
+      <h1>Testeeee</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
-    "./src/components/**.*{js,jsx,ts,tsx,vue}",
-    "./src/pages/**.*.{js,jsx,ts,tsx,vue}",
+    "./src/pages/**/*.{js,jsx,ts,tsx,vue}",
+    "./src/components/**/*.{js,jsx,ts,tsx,vue}",
   ],
   content: [],
   theme: {
