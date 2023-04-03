93df762a40650af5ff351e14448c77c9b87ac422
Alessandro Camargo <acamargodias@terra.com.br>
2023-04-03 09:37:56 -0300

    Ajustes menu

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,24 +1,29 @@
 import Link from "next/link";
 
 interface MenuItemProps {
-  url: string;
   text: string;
   icon: any;
+  url?: string;
+  onClick: (event: any) => {};
 }
 
 export default function MenuItem(props: MenuItemProps) {
-  return (
-    <li className={`hover:bg-gray-100`}>
-      <Link href={props.url}>
-        <a className={`flex flex-col justify-center items-center h-20 w-full`}>
-          {props.icon}
-        </a>
+  function renderLink() {
+    return (
+      <a className={`flex flex-col justify-center items-center h-20 w-full`}>
+        {props.icon}
         <span
           className={`text-xs font-light text-gray-600 flex justify-center`}
         >
           {props.text}
         </span>
-      </Link>
+      </a>
+    );
+  }
+  return (
+    <li className={`hover:bg-gray-100`}>
+      {props.url} ? (<Link href={props.url}>{renderLink()}</Link>
+      ): ( renderLink() )
     </li>
   );
 }
