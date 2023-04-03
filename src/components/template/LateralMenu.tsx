b6d745c9b74e5a5a7803de6cf08b460152c705f9
Alessandro Camargo <acamargodias@terra.com.br>
2023-04-03 09:37:50 -0300

    feat: ajueste menu

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -1,13 +1,22 @@
-import { BellIcon, HomeIcon, SettingsIcon } from "../icons";
+import { BellIcon, HomeIcon, LogoutIcon, SettingsIcon } from "../icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function LateralMenu() {
   return (
-    <aside>
-      <ul>
+    <aside className={`flex flex-col`}>
+      <div
+        className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}
+      >
+        <Logo />
+      </div>
+      <ul className={`flex-grow`}>
         <MenuItem url="/" text="Início" icon={HomeIcon} />
         <MenuItem url="/settings" text="Ajustes" icon={SettingsIcon} />
         <MenuItem url="/notifications" text="Notificaçöes" icon={BellIcon} />
+        <ul className={``}>
+          <MenuItem url="/notifications" text="Sair" icon={LogoutIcon} />
+        </ul>
       </ul>
     </aside>
   );
