ba54eee32d0bea5df2cfed498dcca38bb0564cbb
Alessandro Camargo <acamargodias@terra.com.br>
2023-04-07 22:23:03 -0300

    feat: modo escuro

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -4,7 +4,11 @@ import MenuItem from "./MenuItem";
 
 export default function LateralMenu() {
   return (
-    <aside className={`flex flex-col`}>
+    <aside
+      className={`flex flex-col
+    bg-gray-200 text-gray-200
+    dark:bg-gray-900 `}
+    >
       <div
         className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}
       >
@@ -21,7 +25,7 @@ export default function LateralMenu() {
           text="Sair"
           icon={LogoutIcon}
           onClick={() => console.log("logout")}
-          className={`text-red-600 hover:bg-red hover:bg-red-400 hover:text-white `}
+          className={`text-red-600 dark:text-red-400 hover:bg-red hover:bg-red-400 hover:text-white dark:hover:text-white`}
         />
       </ul>
     </aside>
