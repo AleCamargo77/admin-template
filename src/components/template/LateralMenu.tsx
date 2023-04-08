297f40abea895aae8dbfb1693ea0e3ba2019c257
Alessandro Camargo <acamargodias@terra.com.br>
2023-04-08 15:46:59 -0300

    feat: modo dark

--- src/components/template/LateralMenu.tsx
+++ src/components/template/LateralMenu.tsx
@@ -6,8 +6,8 @@ export default function LateralMenu() {
   return (
     <aside
       className={`flex flex-col
-    bg-gray-200 text-gray-200
-    dark:bg-gray-900 `}
+    bg-gray-200 text-gray-700
+    dark:bg-gray-900 dark:text-gray-200`}
     >
       <div
         className={`flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-800 h-20 w-20`}
