c60289fd0a8a2c39e2d79aad86246552b1fc1dda
Alessandro Camargo <acamargodias@terra.com.br>
2023-04-14 19:52:07 -0300

    feat: modo dark

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,13 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+
+type Theme = "dark" | "";
+interface AppContextProps {
+  theme?: string;
+  changeTheme?: () => void;
+}
 
 const AppContext = createContext({});
 
 export function AppProvider(props: any) {
+  const [theme, setTheme] = useState<Theme>("dark");
+
+  function changeTheme() {
+    setTheme(theme === "" ? "dark" : "");
+  }
   return (
     <div>
       <AppContext.Provider
         value={{
-          nome: "Teste Context API",
+          theme,
+          changeTheme,
         }}
       >
         {props.children}
--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -3,14 +3,14 @@ import Layout from "@/components/template/Layout";
 import useAppData from "@/data/hook/useAppData";
 
 export default function Notifications() {
-  const ctx = useAppData();
+  const { changeTheme } = useAppData();
   return (
     <div>
       <Layout
         title="Notificaçöes"
         subtitle="Aqui vocë irá gerenciar suas notificaçöes"
       >
-        <h3> {ctx.name}</h3>
+        <button onClick={changeTheme}>Alternar Tema</button>
       </Layout>
     </div>
   );
